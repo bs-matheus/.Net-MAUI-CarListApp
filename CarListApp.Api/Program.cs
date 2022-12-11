916c2e47ba36780fcd376f338c4af28120aad384
Matheus Barbosa <matheusbs.silva@outlook.com>
2022-12-11 17:15:13 -0300

    Add database

---
+++ CarListApp.Api/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarListApp.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(o =>
+            {
+                o.AddPolicy("AllowAll", a => a.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            });
+
+            var dbPath = Path.Join(Directory.GetCurrentDirectory(), "carlist.db");
+            var connection = new SqliteConnection($"Data Source={dbPath}");
+            builder.Services.AddDbContext<CarListDbContext>(c => c.UseSqlite(connection));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors("AllowAll");
+
+            app.UseAuthorization();
+
+            app.Run();
+        }
+    }
+}
